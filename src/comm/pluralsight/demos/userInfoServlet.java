95a921d55c0aba38bdba2bc74d94f1c5d7122352
Shiyu Cheng <starkeyshiyu@gmail.com>
2019-08-07 11:28:22 -0700

    initial UserInfoServlet class

---
+++ src/comm/pluralsight/demos/userInfoServlet.java
@@ -0,0 +1,36 @@
+package comm.pluralsight.demos;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class UserInfoServlet extends HttpServlet {
+
+	public UserInfoServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doGet(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(req, resp);
+	}
+	
+	
+
+}
